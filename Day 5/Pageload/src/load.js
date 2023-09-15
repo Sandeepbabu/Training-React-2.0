792cfc760c9c078c0d2386bbad8a3fdf662b9646
Sandeepbabu <30778404+Sandeepbabu@users.noreply.github.com>
2023-09-15 15:26:58 +0530

    Add files via upload

---
+++ Day 5/Pageload/src/load.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import img from "./assets/loading-load.gif"
+import './load.css'
+// import "./Axios.css"
+const Axios = () => {
+const [Data, setData] = useState([]);
+const [loading, setLoading]= useState(true);
+  
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/photos").then((response) => {
+      setData(response.data);
+      console.log(response);
+      setLoading(false);
+     
+    });
+    
+  }, []);
+
+
+  return (
+    <div className="table">
+      
+     {loading ?(
+      <center>
+     <img src={img} alt="loading.."/></center>
+     ):(<div className="col-6">
+        {/* <center> */}
+        <table>
+          <thead>
+            <tr>
+              <th> URL </th>
+              <th> NAME </th>
+            </tr>
+        </thead>
+            {Data.slice(1000).map((getting) => (
+                 <tr key={getting.id}>
+                 <td>{getting.thumbnailUrl}</td>
+                 <td>{getting.url}</td>
+              </tr>
+            
+            ))}
+            </table>
+            {/* </center>   */}
+              </div>
+              )}
+    </div>
+  );
+}
+
+export default Axios;
