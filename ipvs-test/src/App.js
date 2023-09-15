0aa9517ba46b6f5fc19970b92cf41728809f44f0
Sandeepbabu <30778404+Sandeepbabu@users.noreply.github.com>
2023-09-15 15:55:44 +0530

    Add files via upload

---
+++ ipvs-test/src/App.js
@@ -0,0 +1,14 @@
+import Dashboard from "./Dashboard";
+import Sidebar from "./sidebar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
