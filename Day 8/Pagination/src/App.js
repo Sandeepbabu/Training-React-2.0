350a7567c8d60e4ca70efcac36ca7699395f1fc3
Sandeepbabu <30778404+Sandeepbabu@users.noreply.github.com>
2023-09-15 15:32:04 +0530

    Add files via upload

---
+++ Day 8/Pagination/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Pagination from './page';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Pagination />
+
+      
+
+    </div>
+  );
+}
+
+export default App;
